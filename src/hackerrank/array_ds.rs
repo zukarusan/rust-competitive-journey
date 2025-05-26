da31a32df796fb878294de940c469e5520c156a3
Zukarusan <muhammadnabilasykar@gmail.com>
2025-05-26 13:25:45 +0700

    yessir

---
+++ src/hackerrank/array_ds.rs
@@ -0,0 +1,45 @@
+// https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+fn reverseArray(a: &[i32]) -> Vec<i32> {
+    let mut rev = a.to_vec();
+    rev.reverse();
+    rev
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _arr_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let arr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let res = reverseArray(&arr);
+    println!("{}", &arr[0]);
+
+    for i in 0..res.len() {
+        write!(&mut fptr, "{}", res[i]).ok();
+
+        if i != res.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Welcome to your Rust competitive journey");
+}
