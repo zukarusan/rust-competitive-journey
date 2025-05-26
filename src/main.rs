25c4018d262ddf5c79ae36cab02730b952cd1376
Zukarusan <muhammadnabilasykar@gmail.com>
2025-05-26 18:05:16 +0700

    i think i need to solve the problems actually not to make the things more tedious

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
+use std::io::{self, BufRead, Lines, StdinLock};
+
+enum Opt {
+    Load,
+    Exit
+}
+
+fn main()  {
     println!("Welcome to your Rust competitive journey");
+    loop {
+        println!();
+        println!("Select what to do:");
+        println!("1. Load problem from URL");
+        println!("q. Exit");
+        let stdin = io::stdin();
+        let mut iter_in = stdin.lock().lines();
+        
+        let opt = match get_option(&mut iter_in) {
+            Some(o) => o,
+            None => {
+                println!("Please select one of the options only.");
+                continue;
+            }
+        };
+        match opt {
+            Opt::Load => do_load(),
+            Opt::Exit => break
+        }
+    };
+}
+
+fn get_option(iter_in: &mut Lines<StdinLock<'static>>) -> Option<Opt> {
+    iter_in.next()?.ok().map(|opt| match opt.as_str() {
+        "1" => Some(Opt::Load),
+        "q" => Some(Opt::Exit),
+        _ => None
+    })?
+}
+
+fn do_load() {
+    println!("test")
 }
