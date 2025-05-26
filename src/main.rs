1dff04980d3fc76e667adfcd1d975c8bee43871e
Zukarusan <muhammadnabilasykar@gmail.com>
2025-05-26 19:58:08 +0700

    adjust more code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::io::{self, BufRead, Lines, StdinLock};
+use std::io::{self, stdout, BufRead, Lines, StdinLock, Write};
 
 enum Opt {
     Load,
     Exit
 }
 
-fn main()  {
+fn main() {
     println!("Welcome to your Rust competitive journey");
     loop {
-        _ = !_main() && break;
+        _ = _main() && break;
     };
 }
 
@@ -17,23 +17,25 @@ fn _main() -> bool {
     println!("Select what to do:");
     println!("1. Load problem from URL");
     println!("q. Exit");
-    let stdin = io::stdin();
-    let mut iter_in = stdin.lock().lines();
     
-    let opt = match get_option(&mut iter_in) {
+    let opt = match get_option() {
         Some(o) => o,
         None => {
             println!("Please select one of the options only.");
-            return true;
+            return false;
         }
     };
     match opt {
-        Opt::Load => (do_load(), true).1,
-        Opt::Exit => false
+        Opt::Load => (do_load(), false).1,
+        Opt::Exit => true
     }
 }
 
-fn get_option(iter_in: &mut Lines<StdinLock<'static>>) -> Option<Opt> {
+fn get_option() -> Option<Opt> {
+    print!("> ");
+    stdout().flush().ok()?;
+    let stdin = io::stdin();
+    let mut iter_in = stdin.lock().lines();
     iter_in.next()?.ok().map(|opt| match opt.as_str() {
         "1" => Some(Opt::Load),
         "q" => Some(Opt::Exit),
