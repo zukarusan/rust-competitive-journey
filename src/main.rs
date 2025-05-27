59fc2a6cc0c962f100be44879533538d77dfeced
Zukarusan <muhammadnabilasykar@gmail.com>
2025-05-27 17:16:45 +0700

    more on running solutions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::io::{self, stdout, BufRead, Write};
-
+use std::{collections::HashSet, hash::Hash, io::{self, stdout, BufRead, Write}};
 use rust_competitive_journey::hackerrank;
 
+fn vec_to_set<T>(vec: Vec<T>) -> HashSet<T> where T: Eq + Hash {
+    HashSet::from_iter(vec)
+}
 fn main() {
     println!("Welcome to your Rust competitive journey");
     loop {
@@ -10,19 +12,17 @@ fn main() {
 }
 
 fn _main() -> bool {
+    println!();
     println!("================================================");
     println!("Select what to do:");
     println!("1. Load problem from URL");
     println!("2. Run solution");
     println!("q. Exit");
     
-    let Some(opt) = get_option() else {
-        println!("Unable to get option");
-        return false;
-    };
+    let opt = get_option().unwrap_or_default();
     match opt.as_str() {
         "1" => (do_load_problem(), false).1,
-        "2" => (do_run_solution(), false).1,
+        "2" => do_run_solution().unwrap_or(false),
         "q" => true,
         _ => (println!("Please select one of the options only."), false).1
     }
@@ -39,6 +39,36 @@ fn get_option() -> Option<String> {
 fn do_load_problem() {
     println!("test")
 }
-fn do_run_solution() {
-    hackerrank::LIST;
+fn do_run_solution() -> Option<bool> {
+    println!("Select module:");
+    println!("1. HackerRank");
+    print!("> ");
+    stdout().flush().ok()?;
+    let stdin = io::stdin();
+    let opt = {
+        let mut iter_in = stdin.lock().lines();
+        iter_in.next()?.ok()?
+    };
+    Some(match opt.as_str() {
+        "1" => do_run_solution_hackerrank().unwrap_or(false),
+        _ => (println!("Invalid module"), false).1
+    })
+}
+fn do_run_solution_hackerrank() -> Option<bool> {
+    for m in hackerrank::LIST {
+        println!("{}", m);
+    }
+    print!("Type solution to run > ");
+    stdout().flush().ok()?;
+    let stdin = io::stdin();
+    let mut iter_in = stdin.lock().lines();
+    let name = iter_in.next()?.ok()?;
+
+    let names = vec_to_set(hackerrank::LIST.to_vec());
+    if !names.contains(name.as_str()) {
+        println!("There's no solution named {}", name);
+        return Some(false);
+    }
+    
+    Some(true)
 }
