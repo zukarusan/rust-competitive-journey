11cf79a2b2989596c63da4e63eb03d487f308ca2
Zukarusan <muhammadnabilasykar@gmail.com>
2025-05-28 15:45:45 +0700

    add second version of the solution

--- hackerrank/src/bin/s_sparse_array.rs
+++ hackerrank/src/bin/s_sparse_array.rs
@@ -5,6 +5,7 @@ use std::env;
 use std::fs::File;
 use std::hash::Hash;
 use std::io::{self, BufRead, Write};
+use std::iter::TakeWhile;
 
 /*
  * Complete the 'matching_strings' function below.
@@ -14,7 +15,7 @@ use std::io::{self, BufRead, Write};
  *  1. STRING_ARRAY string_list
  *  2. STRING_ARRAY queries
  */
-fn matching_strings(string_list: &[String], queries: &[String]) -> Vec<i32> {
+fn matching_strings(string_list: &[String], queries: &[String]) -> Vec<i32> { // this is faster ig
     let mut answers:Vec<i32> = vec![];
     let mut map = HashMap::<&str, i32>::new();
     for s in string_list {
@@ -30,8 +31,51 @@ fn matching_strings(string_list: &[String], queries: &[String]) -> Vec<i32> {
     answers
 }
 
-fn matching_strings_v2() {
+fn matching_strings_v2(string_list: &[String], queries: &[String]) -> Vec<i32> {
+    let mut answer:Vec<i32> = vec![];
+    let mut string_vec = string_list.to_vec();
+    let mut map = HashMap::<&str, i32>::new();
     
+    string_vec.sort_unstable();
+    for q in queries {
+        map.entry(q)
+        .and_modify(|a| {
+            answer.push(*a);
+        })
+        .or_insert_with(|| {
+            if let Ok(idx) = string_vec.binary_search(q) {
+                let mut count = 1;
+                count += v2_count_occurence(&string_vec[..], &q, idx, true);
+                count += v2_count_occurence(&string_vec[..], &q, idx, false);
+                answer.push(count);
+                count
+            } else {
+                answer.push(0);
+                0
+            }
+        });
+    }
+    answer
+}
+
+fn v2_count_occurence(arr: &[String] , find: &str, idx: usize, left_or_right: bool) -> i32 {
+    let len = arr.len();
+    let step = if left_or_right { -1i32 } else { 1 };
+    let start = idx as i32 + step;
+    let mut i = start;
+    let mut count = 0;
+    loop {
+        let _i = i as usize;
+        if i < 0 || _i >= len {
+            break;
+        }
+        if arr[_i] != find {
+            break;
+        }
+        count += 1;
+        i += step;
+    }
+    count
 }
 
 fn main() {
@@ -58,7 +102,7 @@ fn main() {
         queries.push(queries_item);
     }
 
-    let res = matching_strings(&string_list, &queries);
+    let res = matching_strings(&string_list, &queries); // by this we know that the simplest approach "often" means the correct one
 
     for i in 0..res.len() {
         write!(&mut fptr, "{}", res[i]).ok();
