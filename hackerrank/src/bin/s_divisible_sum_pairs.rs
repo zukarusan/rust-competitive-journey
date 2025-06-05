f9437586289b0cc2e7246e131a89578fe71d0910
Zukarusan <muhammadnabilasykar@gmail.com>
2025-06-05 17:44:19 +0700

    divisible sum pairs

---
+++ hackerrank/src/bin/s_divisible_sum_pairs.rs
@@ -0,0 +1,54 @@
+// https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'divisible_sum_pairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+fn divisible_sum_pairs(n: i32, k: i32, ar: &[i32]) -> i32 {
+    let mut count = 0;
+    for i in 0..(n-1) as usize {
+        let a_i = ar[i];
+        for j in i+1..n as usize {
+            if (a_i + ar[j]) % k == 0 {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let n = first_multiple_input[0].trim().parse::<i32>().unwrap();
+
+    let k = first_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let ar: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = divisible_sum_pairs(n, k, &ar);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
