8d663e37cae52a3f08d4e5ec14db3bfa50dd3327
Zukarusan <muhammadnabilasykar@gmail.com>
2025-06-05 18:37:46 +0700

    linked list 1

---
+++ hackerrank/src/bin/s_insert_a_node_at_the_tail_of_a_linked_list.rs
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true
+
+use std::{collections::LinkedList, env, fs::File, io::{self, BufRead, Write}};
+
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut file = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+    
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let mut list: LinkedList<i32> = LinkedList::new();
+    for _ in 0..n {
+        let el = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+        list.push_back(el);
+    }
+    list.iter().for_each(|x| {
+        writeln!(&mut file, "{}", x).ok();
+    });
+}
