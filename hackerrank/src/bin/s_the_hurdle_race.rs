af1c6ce50d83614b325f8a83638f881d5d360350
Zukarusan <muhammadnabilasykar@gmail.com>
2025-06-30 17:43:19 +0700

    another simple problem

---
+++ hackerrank/src/bin/s_the_hurdle_race.rs
@@ -0,0 +1,44 @@
+// https://www.hackerrank.com/challenges/the-hurdle-race/problem?isFullScreen=true
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'hurdle_race' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+fn hurdle_race(k: i32, height: &[i32]) -> i32 {
+    (height.iter().max().unwrap_or(0) - k).max(0)
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let n = first_multiple_input[0].trim().parse::<i32>().unwrap();
+
+    let k = first_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let height: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = hurdle_race(k, &height);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
